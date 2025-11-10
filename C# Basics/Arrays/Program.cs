f56144f2d36defd985d4f201424347d6fa3bb22e
Jai-Irkal <jai.vinodirkal@emcus.co.in>
2025-11-10 13:34:55 +0530

    Arrays in C#

---
+++ C# Basics/Arrays/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            numbers[2] = 30;
+
+            string[] friends = new string[6];
+            friends[0] = "Jai";
+            friends[1] = "Anas";
+            friends[2] = "Shameem";
+        }
+    }
+}
