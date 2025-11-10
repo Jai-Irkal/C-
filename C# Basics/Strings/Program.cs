59137b10ce248bca159aa43aea31bb6ea0568b7c
Jai-Irkal <jai.vinodirkal@emcus.co.in>
2025-11-10 12:55:21 +0530

    Working with Strings

--- C# Basics/Strings/Program.cs
+++ C# Basics/Strings/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Hello this is an " + "Example string";
+            Console.WriteLine(text);
+            // Length of string
+            Console.WriteLine(text.Length);
+            // UpperCase
+            Console.WriteLine(text.ToUpper());
+            // LowerCase
+            Console.WriteLine(text.ToLower());
+            // Check substring exists
+            Console.WriteLine(text.Contains("Hello"));
+            // Check index of existing substring
+            Console.WriteLine(text.IndexOf("this")); // If not exists -1
+            
+        }
+    }
+}
