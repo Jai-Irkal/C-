fe59d6d24ebfe7bc5a7ad4ad248223a1fdffa2d6
Jai-Irkal <jai.vinodirkal@emcus.co.in>
2025-11-10 13:42:37 +0530

    Methods in C#

---
+++ C# Basics/Methods/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Greet("Jai");
+            Console.ReadLine();
+        }
+        
+        // create method
+        static void Greet(string name)
+        {
+            Console.WriteLine("Hello "+ name);
+        }
+    }
+}
