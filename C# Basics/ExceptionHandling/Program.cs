1641415369b040003487be52aab4cf8673b5af28
Jai-Irkal <jai.vinodirkal@emcus.co.in>
2025-11-10 14:12:13 +0530

    Exception handling in C#

---
+++ C# Basics/ExceptionHandling/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter Number: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter another number: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(num1 / num2);
+                Console.ReadLine();
+            }
+            catch (DivideByZeroException e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Exited");
+            }
+        }
+    }
+}
